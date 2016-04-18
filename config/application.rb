2148633c2aa089ba90865ddeff50c80f146ffd3c
Nicolas Florentin <nicolas@sleede.com>
2016-04-18 16:43:46 +0200

    require elasticsearch/rails/instrumentation to logs elastic-related action

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'elasticsearch/rails/instrumentation'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 module Openfablab
   class Application < Rails::Application
