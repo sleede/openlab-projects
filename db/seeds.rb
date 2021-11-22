ce9b0b23037e98f68b5a82a4a20ab51837adbfe0
Sylvain <sylvain@sleede.com>
2021-11-22 16:14:36 +0100

    move dev env to docker-compose + improved dev env setup instructions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-#encoding: utf-8
+# frozen_string_literal: true
 
 if User.count == 0
-  User.create!(email: 'admin@sleede.com', password: 'kikoulol', password_confirmation: 'kikoulol')
+  User.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD'])
 end
